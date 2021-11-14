e2725da1caeb4764ddf6065fc1c4e6eaa123c5e6
listertuk <33751400+listertuk@users.noreply.github.com>
2021-11-14 17:09:04 +0000

    Update and rename ShoptectConstants.groovy to SubTasksConstants.groovy

---
+++ com.domain1.retail/com/domain1/retail/SubTasksConstants.groovy
@@ -0,0 +1,9 @@
+package com.domain1.retail
+// used by subtask project workflows
+class SubTasksConstants {
+    
+    public final static String FIELD = "subtask.flag.field"
+    public final static String LABEL = "subtask.label"
+    public final static String SUBTASKS = "subtask.sub-tasks"
+    
+}
