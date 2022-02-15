6772dda31b8ac45221dab2bb1c5285bcfc6cf046
listertuk <listertuk@gmail.com>
2022-02-15 20:58:18 +0000

    rename and document

--- Scriptrunner/Admin/AddComponentToAllListedProjects.groovy
+++ Scriptrunner/Admin/AddComponentToAllListedProjects.groovy
@@ -1,42 +1,29 @@
+/*
+ bulk add a list of components to a list of projects where all share same functionality
+*/
 import com.atlassian.jira.component.ComponentAccessor
 import com.atlassian.jira.bc.project.component.ProjectComponent
 import com.atlassian.jira.bc.project.component.ProjectComponentManager
 
-/* new component */
-//String componentName = "EE"
-//String description = "desc"
 long projectId = 0
 
 ProjectComponentManager pComMgr = ComponentAccessor.getProjectComponentManager()
 ProjectComponent component
 def projectManager = ComponentAccessor.getProjectManager()
 
-String[] projects = ["WSC20200020",
-"WSC20200001",
-"WSC20200019",
-"WSC20200021",
-"WSC20200022"
+String[] projects = ["PROJ1",
+"PROJ2",
+"PROJ3"
 ]
 					 
-String[] components = ["01 Task - Issue & Inquiry Only",
-"02 PD Registration",
-"03 PD Modification",
-"04 PD Update (Spec)",
-"05 MKT PD Creation",
-"06 MKT PD Modification",
-"07 QA",
-"08 Static Page",
-"09 Offer & Promotion",
-"10 Home Page",
-"11 GNB",
-"12 Footer",
-"13 PCD / PFS / PF",
-"14 About Us",
-"15 Accessibility",
-"16 Info - Sitemap",
-"17 DTM Rule",
-"gnm",
-"homepage"
+String[] components = ["Issue & Inquiry Only",
+"Registration",
+"Modification",
+"Update",
+"Static Page",
+"Promotion",
+"Home Page",
+"Footer"
 ]
 					 
 
