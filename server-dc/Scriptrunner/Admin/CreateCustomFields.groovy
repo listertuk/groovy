af148bb45f456237644285e11f20858192cf593b
listertuk <listertuk@gmail.com>
2022-10-22 16:27:12 +0100

    tidy up

--- server-dc/Scriptrunner/Admin/CreateCustomFields.groovy
+++ server-dc/Scriptrunner/Admin/CreateCustomFields.groovy
@@ -32,27 +32,27 @@ String TEXT = "com.atlassian.jira.plugin.system.customfieldtypes:textfield"
 String TEXT_SEARCHER = "com.atlassian.jira.plugin.system.customfieldtypes:textsearcher"
 String SINGLE_SELECT = "com.atlassian.jira.plugin.system.customfieldtypes:select"
 String SINGLE_SELECT_SEARCHER = "com.atlassian.jira.plugin.system.customfieldtypes:multiselectsearcher"
-	
+    
 
-		List<IssueType> issueTypes = new ArrayList<IssueType>();
-		//issueTypes.add(null);
+        List<IssueType> issueTypes = new ArrayList<IssueType>();
+        //issueTypes.add(null);
 
-		// Create a list of project contexts for which the custom field needs to be
-		// available
-		List<JiraContextNode> contexts = new ArrayList<JiraContextNode>();
-		JiraContextNode node
+        // Create a list of project contexts for which the custom field needs to be
+        // available
+        List<JiraContextNode> contexts = new ArrayList<JiraContextNode>();
+        JiraContextNode node
        // add B2B/B2C
-        def fields = ["Retail Internal Brief Flag",
-"Retail Planning and Design Flag",
-"Retail Prototype Flag",
-"Retail Production Flag",
-"Retail Logistics Flag",
-"Retail Installation Flag",
-"Retail Snagging Flag",
-"Retail Reporting Flag"]
+        def fields = ["Internal Brief Flag",
+"Planning and Design Flag",
+"Prototype Flag",
+"Production Flag",
+"Logistics Flag",
+"Installation Flag",
+"Snagging Flag",
+"Reporting Flag"]
 fields.each() {String name ->
-		String[] values = ["Y"]	
-		def cField = createOptionField(name, "", issueTypes, 
+        String[] values = ["Y"]    
+        def cField = createOptionField(name, "", issueTypes, 
                                        contexts, values, customFieldManager, fieldScreenManager, optionsManager, SINGLE_SELECT,SINGLE_SELECT_SEARCHER);
 
 }
@@ -63,94 +63,94 @@ fields.each() {String name ->
 // TODO set default select option value
 
 public CustomField createTextField(String name, String description, List<IssueType> issueTypes,
-			List<JiraContextNode> contexts, CustomFieldManager customFieldManager, FieldScreenManager fieldScreenManager, String type, String searcher) throws GenericEntityException {
-		CustomField cField = customFieldManager.getCustomFieldObject(name);
-		if (!cField) {
-			cField = customFieldManager.createCustomField(name, description,
-					customFieldManager
-							.getCustomFieldType(type),
-					customFieldManager
-							.getCustomFieldSearcher(searcher),
+            List<JiraContextNode> contexts, CustomFieldManager customFieldManager, FieldScreenManager fieldScreenManager, String type, String searcher) throws GenericEntityException {
+        CustomField cField = customFieldManager.getCustomFieldObject(name);
+        if (!cField) {
+            cField = customFieldManager.createCustomField(name, description,
+                    customFieldManager
+                            .getCustomFieldType(type),
+                    customFieldManager
+                            .getCustomFieldSearcher(searcher),
 
-					contexts, issueTypes);
-            		addToDefaultScreen(cField, fieldScreenManager);
+                    contexts, issueTypes);
+                    addToDefaultScreen(cField, fieldScreenManager);
 
-		}
-		return cField;
-	}
+        }
+        return cField;
+    }
 
-	public CustomField createMultiCheckbox(String name, String description, List<IssueType> issueTypes,
-			List<JiraContextNode> contexts, String[] options, CustomFieldManager customFieldManager, FieldScreenManager fieldScreenManager, OptionsManager optionsManager,
+    public CustomField createMultiCheckbox(String name, String description, List<IssueType> issueTypes,
+            List<JiraContextNode> contexts, String[] options, CustomFieldManager customFieldManager, FieldScreenManager fieldScreenManager, OptionsManager optionsManager,
                                           String type, String searcher)
-			throws GenericEntityException {
+            throws GenericEntityException {
                 createOptionField(name, description, issueTypes,
-			contexts, options,  customFieldManager,  fieldScreenManager,  optionsManager, type, searcher)
+            contexts, options,  customFieldManager,  fieldScreenManager,  optionsManager, type, searcher)
             }
 
-	public CustomField createMultiSelect(String name, String description, List<IssueType> issueTypes,
-			List<JiraContextNode> contexts, String[] options, CustomFieldManager customFieldManager, FieldScreenManager fieldScreenManager, OptionsManager optionsManager,
+    public CustomField createMultiSelect(String name, String description, List<IssueType> issueTypes,
+            List<JiraContextNode> contexts, String[] options, CustomFieldManager customFieldManager, FieldScreenManager fieldScreenManager, OptionsManager optionsManager,
                                         String type, String searcher)
-			throws GenericEntityException {
+            throws GenericEntityException {
                 createOptionField(name, description, issueTypes,
-			contexts, options,  customFieldManager,  fieldScreenManager,  optionsManager, type, searcher)
+            contexts, options,  customFieldManager,  fieldScreenManager,  optionsManager, type, searcher)
             }
 
-	public  CustomField createOptionField(String name, String description, List<IssueType> issueTypes,
-			List<JiraContextNode> contexts, String[] options, CustomFieldManager customFieldManager, FieldScreenManager fieldScreenManager, OptionsManager optionsManager,
+    public  CustomField createOptionField(String name, String description, List<IssueType> issueTypes,
+            List<JiraContextNode> contexts, String[] options, CustomFieldManager customFieldManager, FieldScreenManager fieldScreenManager, OptionsManager optionsManager,
                                          String type, String searcher)
-			throws GenericEntityException {
-		CustomField cField = customFieldManager.getCustomFieldObject(name);
-		if (!cField) {
-			cField = customFieldManager.createCustomField(name, description,
-					customFieldManager
-							.getCustomFieldType(type),
-					customFieldManager.getCustomFieldSearcher(searcher),
+            throws GenericEntityException {
+        CustomField cField = customFieldManager.getCustomFieldObject(name);
+        if (!cField) {
+            cField = customFieldManager.createCustomField(name, description,
+                    customFieldManager
+                            .getCustomFieldType(type),
+                    customFieldManager.getCustomFieldSearcher(searcher),
 
-					contexts, issueTypes);
+                    contexts, issueTypes);
             addToDefaultScreen(cField, fieldScreenManager);
             for(value in options) {
                 addOptionToCustomField(cField, value, optionsManager)
             }
-		}
-		return cField;
-	}
+        }
+        return cField;
+    }
 
-	public  void addToDefaultScreen(CustomField cField, FieldScreenManager fieldScreenManager) {
-		// Add field to default Screen
-		FieldScreen defaultScreen = fieldScreenManager.getFieldScreen(FieldScreen.DEFAULT_SCREEN_ID);
-		if (!defaultScreen.containsField(cField.getId())) {
+    public  void addToDefaultScreen(CustomField cField, FieldScreenManager fieldScreenManager) {
+        // Add field to default Screen
+        FieldScreen defaultScreen = fieldScreenManager.getFieldScreen(FieldScreen.DEFAULT_SCREEN_ID);
+        if (!defaultScreen.containsField(cField.getId())) {
 
-			FieldScreenTab firstTab = defaultScreen.getTab(0);
-			firstTab.addFieldScreenLayoutItem(cField.getId());
-		}
-	}
-	public Option addOptionToCustomField(CustomField customField, String value,OptionsManager optionsManager) {
-		Option newOption = null;
+            FieldScreenTab firstTab = defaultScreen.getTab(0);
+            firstTab.addFieldScreenLayoutItem(cField.getId());
+        }
+    }
+    public Option addOptionToCustomField(CustomField customField, String value,OptionsManager optionsManager) {
+        Option newOption = null;
 
-		if (customField != null) {
-			List<FieldConfigScheme> schemes = customField
-					.getConfigurationSchemes();
-			if (schemes != null && !schemes.isEmpty()) {
-				FieldConfigScheme sc = schemes.get(0);
-				Map configs = sc.getConfigsByConfig();
-				if (configs != null && !configs.isEmpty()) {
-					FieldConfig config = (FieldConfig) configs.keySet()
-							.iterator().next();
+        if (customField != null) {
+            List<FieldConfigScheme> schemes = customField
+                    .getConfigurationSchemes();
+            if (schemes != null && !schemes.isEmpty()) {
+                FieldConfigScheme sc = schemes.get(0);
+                Map configs = sc.getConfigsByConfig();
+                if (configs != null && !configs.isEmpty()) {
+                    FieldConfig config = (FieldConfig) configs.keySet()
+                            .iterator().next();
                     
                    
 
-					//OptionsManager optionsManager = getOptionsManager();
-					//Options l = optionsManager.getOptions(config);
-					//Option opt;
+                    //OptionsManager optionsManager = getOptionsManager();
+                    //Options l = optionsManager.getOptions(config);
+                    //Option opt;
 
-					int numberAdded = 100;
-					newOption = optionsManager.createOption(config, null,
-							new Long(numberAdded), // TODO What is this
-							value);
+                    int numberAdded = 100;
+                    newOption = optionsManager.createOption(config, null,
+                            new Long(numberAdded), // TODO What is this
+                            value);
                     
-				}
-			}
-		}
+                }
+            }
+        }
 
-		return newOption;
-	}
+        return newOption;
+    }
