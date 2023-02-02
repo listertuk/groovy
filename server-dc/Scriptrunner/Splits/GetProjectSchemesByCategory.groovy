ea98ba82a61f07b8889eee1618ed1b7adbc7b0e5
Listert <listertuk@gmail.com>
2023-02-02 14:52:24 +0000

    revert category version

--- server-dc/Scriptrunner/Splits/GetProjectSchemesByCategory.groovy
+++ server-dc/Scriptrunner/Splits/GetProjectSchemesByCategory.groovy
@@ -1,38 +1,41 @@
 import com.atlassian.jira.component.ComponentAccessor
 import org.apache.log4j.Category
-
-import com.atlassian.jira.project.ProjectManager
-
 import com.atlassian.jira.project.Project
 
 def issueTypeScreenSchemeManager=ComponentAccessor.getIssueTypeScreenSchemeManager()
 
+//def loggedInUser = ComponentAccessor.jiraAuthenticationContext.loggedInUser
 def Category log = Category.getInstance("com.onresolve.jira.groovy.PostFunction")
 log.setLevel(org.apache.log4j.Level.DEBUG)
 
+String [] categories = ["MIGRATE"] 
 
-String [] projectKeys = ['KAN1', 'NR2', 'NR3']
-ProjectManager pMgr = ComponentAccessor.projectManager 
+//gives info about projects
+def pMgr = ComponentAccessor.getProjectManager() //.getProjectObjects()
+//Project prj = ComponentAccessor.getProjectManager().getProjectObjByKey("HCI")
+//def issueTypeSchemeManager = ComponentAccessor.getIssueTypeSchemeManager()
 def issueTypeScreenScheme = ComponentAccessor.getIssueTypeScreenSchemeManager()
 def wrkflwScheme = ComponentAccessor.getWorkflowSchemeManager()
 def fieldScheme = ComponentAccessor.getFieldLayoutManager()
 log.info("Project Name,Project Id,Issue type Screen scheme,Issue types,Workflow Scheme,Screen SchemeField Config Scheme ")
-projectKeys.each() {pKey ->
-    Project project = pMgr.getProjectObjByKey(pKey) //.getProjectObjectsFromProjectCategory(cat.getId())
-    if (project) {
-      
-    log.info(""+project.getName() +","+ project.getId() +","+ issueTypeScreenSchemeManager.getIssueTypeScreenScheme(project).getName() +","+ project.getIssueTypes()*.name +","+ 
-        wrkflwScheme.getWorkflowSchemeObj(project).getName() +","+ issueTypeScreenScheme.getIssueTypeScreenScheme(project).getName() +","+ fieldScheme.getFieldConfigurationScheme(project)?.getName())
-//log.info("Project Name is: " +project.getName())
-//log.info("Project Id of Long type is: " +project.getId()) 
-//log.info ("Issue type Screen scheme is :"+issueTypeScreenSchemeManager.getIssueTypeScreenScheme(project).getName())
-//log.info("Issue types associated with project is: " +project.getIssueTypes()*.name)
-//log.info("Workflow Scheme for Project: " +project.getName().toString() +" is: "+wrkflwScheme.getWorkflowSchemeObj(project).getName())
-//log.info("Screen Scheme is :" +issueTypeScreenScheme.getIssueTypeScreenScheme(project).getName()) 
-//log.info("Field Config Scheme is :" +fieldScheme.getFieldConfigurationScheme(project)?.getName())
+categories.each() {category ->
+      def cat = pMgr.getProjectCategoryObjectByName(category)
+    // get all projects for category
+    def allProjects = pMgr.getProjectObjectsFromProjectCategory(cat.getId())
+   
+      allProjects.each{it->
+
+      Project p =ComponentAccessor.getProjectManager().getProjectObj(it.getId())
+
+      log.info(""+it.getName() +","+ it.getId() +","+ issueTypeScreenSchemeManager.getIssueTypeScreenScheme(p).getName() +","+ it.getIssueTypes()*.name +","+ 
+      wrkflwScheme.getWorkflowSchemeObj(p).getName() +","+ issueTypeScreenScheme.getIssueTypeScreenScheme(p).getName() +","+ fieldScheme.getFieldConfigurationScheme(p)?.getName())
+//log.info("Project Name is: " +it.getName())
+//log.info("Project Id of Long type is: " +it.getId()) 
+//log.info ("Issue type Screen scheme is :"+issueTypeScreenSchemeManager.getIssueTypeScreenScheme(p).getName())
+//log.info("Issue types associated with project is: " +it.getIssueTypes()*.name)
+//log.info("Workflow Scheme for Project: " +it.getName().toString() +" is: "+wrkflwScheme.getWorkflowSchemeObj(p).getName())
+//log.info("Screen Scheme is :" +issueTypeScreenScheme.getIssueTypeScreenScheme(p).getName()) 
+//log.info("Field Config Scheme is :" +fieldScheme.getFieldConfigurationScheme(p)?.getName())
 //log.info('---------------------------------------------------------------------') 
-} else {
-      log.info(pKey +  " not found")
-}
-      
+      }
 }
